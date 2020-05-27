c77a425744e31ae083cfef829960283c237532b5
Nick Walton <nickwalton00@gmail.com>
2020-05-27 17:14:41 -0600

    Delete quest.js

--- examples/quest.js
+++
@@ -1,8 +0,0 @@
-module.exports = async function({text}) {
-    if(!state?.quests){
-        state.quests = {current: 'kill a dragon'}
-    }
-    const completed = await utils.checkQuestCompleted({history, quest: state.quests.current, text})
-    state.message = 'Completed your quest? ' + completed
-    return {text}
-}
