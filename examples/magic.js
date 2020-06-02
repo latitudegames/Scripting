9a64f71ba583029adf3f9968e111996392a80b33
Nick Walton <nickwalton00@gmail.com>
2020-06-01 20:23:02 -0600

    Update magic.js

--- examples/magic.js
+++ examples/magic.js
@@ -1,6 +1,7 @@
 // Here's a fun scripting example where players have to learn these magic spells to have cool effects.
 // The world info has entries that should hopefully lead people to these spells and so that they can find and cast them.
 // Can find the scenario at https://play.aidungeon.io/scenario/e982a8f0-a473-11ea-af38-d1932fa9d9e0
+// I changed the spell names so it doesn't ruin the discovery if you play the adventure.
 
 const modifier = (text) => {
   
@@ -12,9 +13,9 @@ const modifier = (text) => {
   }
   
   const spells = {
-    'bazinga': 'a deathly fire ball spell that',
-    'eleno': 'turning yourself into a cloud allowing you to move at will',
-    'demonia': 'a dark spell that summons an evil demon. You hear a dark rumbling and see a cloud of black smoke appear. Out of it appears a large horned demon'
+    'SPELL1': 'a deathly fire ball spell that',
+    'SPELL2': 'turning yourself into a cloud allowing you to move at will',
+    'SPELL3': 'a dark spell that summons an evil demon. You hear a dark rumbling and see a cloud of black smoke appear. Out of it appears a large horned demon'
   }
   
   const lowered = text.toLowerCase()
