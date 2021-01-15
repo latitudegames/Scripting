4fdd036010f829562aff037efaf9e34ccbb8fe87
Matt Brockman <mattbrockman@gmail.com>
2021-01-15 09:22:22 -0800

    evaluationBot usage

---
+++ examples/evaluationBots.js
@@ -0,0 +1,30 @@
+
+// Checkout the repo examples to get an idea of other ways you can use scripting 
+// https://github.com/latitudegames/Scripting/blob/master/examples
+
+//CONTEXT MODIFIER
+const modifier = (text) => {
+  state.evaluationBot = 'JudgeBot'
+return { text }
+}
+
+// Don't modify this part
+modifier(text)
+
+
+// OUTPUT MODIFIER
+const modifier = (text) => {
+  console.log(info)
+return { text }
+}
+
+// Don't modify this part
+modifier(text)
+
+
+
+/*
+  AVAILABLE BOTS
+
+  
+*/
