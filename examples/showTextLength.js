d1118f8230ca9f4d7618285f02adcdd2f5635997
Matt Brockman <mattbrockman@gmail.com>
2021-01-14 17:51:08 -0800

    Update showTextLength.js

--- examples/showTextLength.js
+++ examples/showTextLength.js
@@ -2,6 +2,8 @@
 // Checkout the repo examples to get an idea of other ways you can use scripting 
 // https://github.com/latitudegames/Scripting/blob/master/examples
 
+
+// OUTPUT MODIFIER
 const modifier = (text) => {
   
     state.displayStats = [{key:'Text Length', value: text.length, color: 'red'}]
