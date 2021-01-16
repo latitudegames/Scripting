a4dd047b323839062938462d38cbd21b21db1a90
Matt Brockman <mattbrockman@gmail.com>
2021-01-15 21:17:38 -0800

    simplify example

--- examples/skills.js
+++ examples/skills.js
@@ -1,27 +1,6 @@
 //input modifiers to set up skills
 
-
-//EXAMPLE 1
-//creates skills with 0 skill points
-const modifier = (text) => {    
-    state.skills = {'turtle':1}
-  }
-  
-  // Don't modify this part
-  modifier(text)
-  
-//EXAMPLE 2
-//creates skills with 1 skill point
-const modifier = (text) => {    
-    state.skills = {'turtle':1}
-    state.skillPoints = 1
-  }
-  
-  // Don't modify this part
-  modifier(text)
-  
-//EXAMPLE 3
-//creates skills with 5 skill points and 
+//creates skills with 5 skill points (for input modifier) 
 const modifier = (text) => {    
     state.skills = {'turtle':1}
     state.skillPoints = 1
