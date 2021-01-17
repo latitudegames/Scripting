4e35aa46a49043385b0703d24ee5e6b183d39dea
Matt Brockman <mattbrockman@gmail.com>
2021-01-16 17:55:15 -0800

    stats toy example

---
+++ examples/stats.js
@@ -0,0 +1,11 @@
+//input modifiers to set up stats
+
+// creates stats to open up the stats overlay
+const modifier = (text) => {    
+    state.stats = {stats:{strength:{level: 4, cost:2}, agility:{level: 1, cost:5},}, statPoints:50}
+    return ({text})
+  }
+  
+  // Don't modify this part
+  modifier(text)
+  
